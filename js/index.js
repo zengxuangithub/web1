43a4f48d7bbf5aa624ac52631c96535762a6f004
zengxuangithub <649590593@qq.com>
2017-07-24 18:07:37 +0800

    170724

--- js/index.js
+++ js/index.js
@@ -60,33 +60,41 @@ window.onload = function()
 	myscrollUl.addEventListener("touchmove",ulSmove,false);
 	myscrollUl.addEventListener("touchend",ulEnd,false);
 	var myli=myscrollUl.getElementsByTagName("li");
-	  console.log("with----------->"+myli[0].offsetWidth); 
-	 
-	var myleft=myscrollUl.offsetLeft;
+	  //console.log("with----------->"+myli[0].offsetWidth); 
 	
+	var myleft=myscrollUl.offsetLeft;
+	var isend=false;
+	var isstart=true;
+	var leng;
 	var startx,endx;
+	var ulLeftpostion=0;
 	function ulStart(event)
 	{
+		
+		
 		 var touch = event.touches[0];
-	    
+	    event.preventDefault();
            startx = touch.pageX;
+          // ulLeftpostion=myscrollUl.offsetLeft;
            
 	}
 	
 	
 	function ulSmove(event)
 	{
+		event.preventDefault();
 		 var touch = event.touches[0];
                 
                  //myleft=myscrollUl.offsetLeft;
                 endx=touch.pageX;
                 //myleft=myscrollUl.offsetLeft;
-                var leng=touch.pageX-startx;
-              console.log("myleft----------->"+myleft); 
+                 leng=touch.pageX-startx;
+            
                 if(leng>0)
                 {
                 	//向右滑
                 	   //console.log("右边move----------->"+leng); 
+                	   
                 	myscrollUl.style.left=myleft+leng+"px";
                 }
                 else
@@ -100,16 +108,135 @@ window.onload = function()
               
 	}
 	
+	var adsrollTime;
+	var backTime;
 	 function ulEnd(event) 
 	 {
+	 	event.preventDefault();
 	 	myleft=myscrollUl.offsetLeft;
-               // console.log("startx-----------"+startx); 
-               // console.log("endx-----------"+endx);
-               // myscrollUl.style.left=
-              // var touch =event.touches[0];
+	 	
+	 	
+	 	
+	 	if(leng>0)
+	 	{
+	 			 
+	 			 if(isstart)
+	 			 {
+	 			 		console.log("isstart----------->"+myscrollUl.offsetLeft); 
+	 			 		myscrollUl.style.left =0+'px'; 
+	 			 		myleft=0;
+	 			 		return;
+	 			 	//backmove(1);
+	 			 }
+	 				if(leng<50)
+                	  
+       				 	 {
+       				  		backTime =setTimeout(backmove(1),1);
+                	   	
+        				}
+                	   else
+      					{
+        	        	  if(leng>600) 
+        	        	  {
+        	        	  	ulLeftpostion=600+ulLeftpostion;
+        	        	  	backTime =setTimeout(backmove(1),1);
+        	        	  }
+        	        	  else
+        	        	  {
+        	        	  	ulLeftpostion=600+ulLeftpostion;
+        	        	  	 //console.log("myleft----------->"+(600+ulLeftpostion)); 
+        	        	  	backTime =setTimeout(backmove(0),1);
+        	        	  }
+        			 	}
+	 	}
+	 	else
+	 	{
+	 		
+	 			if(isend)
+	 			 {
+	 			 		console.log("isend----------->"+myscrollUl.offsetWidth); 
+	 			 		myscrollUl.style.left =-myscrollUl.offsetWidth+600+'px'; 
+	 			 		myleft=-2400;
+	 			 		return;
+	 			 }
+	 		
+	 				if(leng>-50)
+                	  
+       				  {
+       				  		backTime =setTimeout(backmove(1),1);
+                	   	
+        				}
+                	   else
+      					{
+        	        	  if(leng<-600) 
+        	        	  {
+        	        	  	ulLeftpostion=-600+ulLeftpostion;
+        	        	  	backTime =setTimeout(backmove(0),1);
+        	        	  }
+        	        	  else
+        	        	  {
+        	        	  	ulLeftpostion=-600+ulLeftpostion;
+        	        
+        	        	  	//backTime =setTimeout(backmove(1),1);
+        	        	  	backmove(1);
+        	        	  	//console.log("ulEnd----------->"+myscrollUl.offsetLeft); 
+        	        	  }
+        			 	}
+	 	}
+	 
+            
               
                 
       }
+	 
+	 
+	 function backmove(isleft)
+	 {
+	 	
+	 
+	 	if(myscrollUl.offsetLeft==ulLeftpostion)  
+	 	{
+	 		console.log("stop----------->"+myscrollUl.offsetLeft); 
+	 		clearTimeout(backTime);
+	 		ulLeftpostion=myscrollUl.offsetLeft;
+	 		myleft=myscrollUl.offsetLeft;
+	 		if(ulLeftpostion==-2400)
+	 		{
+	 			isend=true;
+	 			isstart=false;
+	 		}
+	 		
+	 	 else	if(ulLeftpostion==0)
+	 		{
+	 			isend=false;
+	 			isstart=true;
+	 		}
+	 		else
+	 		{
+	 			isend=false;
+	 			isstart=false;
+	 		}
+	 		return;
+	 	}
+	 	if(isleft==1)
+	 	{
+	 		myscrollUl.style.left = (myscrollUl.offsetLeft -1)+'px'; 
+	 		console.log("myleft----------->"+myscrollUl.offsetLeft);
+	 		backTime =setTimeout(backmove(1),30);
+	 		
+	 	}
+	 	else
+	 	{
+	 		console.log("offsetLeft----------->"+myscrollUl.offsetLeft); 
+	 		myscrollUl.style.left = myscrollUl.offsetLeft +1 + 'px';
+	 		backTime =setTimeout(backmove(0),1);
+	 			 
+	 	}
+	 	
+	 	
+	 }
+	 
+	
 	
 	
 	
