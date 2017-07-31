cdf13319a65f0323a2dbb1a8e617a6264c499fee
zengxuangithub <649590593@qq.com>
2017-07-31 17:50:42 +0800

    add login

---
+++ API/getAdminInfo.php
@@ -0,0 +1,36 @@
+<?php
+
+	header("Content-Type: text/json;charset=utf-8");
+	header("Access-Control-Allow-Origin: *"); 
+	if(!isset($_GET["user"]))
+	{
+		
+		$result=array('error'=>'0','info'=>'参数异常');
+		echo $result;
+		die();
+		
+		
+	}
+	if(isset($_GET["user"])&&!isset($_GET["pwd"]))
+	{
+		//校验用户名
+		
+		$logoStr=array('img'=>'http://pic122.nipic.com/file/20170221/23749051_142920258000_2.jpg','tag'=>'logoPic');
+	
+	//echo $_GET['callback']."(".json_encode($logoStr).")";
+	echo json_encode($logoStr);
+		
+		//$result=array('error'=>'0','info'=>'校验用户名');
+		//echo $result;
+		die();
+		
+	}
+	if(isset($_GET["user"])&&isset($_GET["pwd"]))
+	{
+		//校验用户名
+		$result=array('error'=>'0','info'=>'ok');
+		echo $result;
+		die();
+	}
+	
+?>
